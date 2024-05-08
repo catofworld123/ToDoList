7918ab33dcd2d98bb1dade6b45f47b10fab570bd
Arminias <arminias@t-online.de>
2024-05-08 07:56:20 +0200

    Fix for non-existing legacy remapped file

--- install.bat
+++ install.bat
@@ -5,7 +5,9 @@ call gradlew.bat --no-daemon downloadAssets
 mkdir BTW_dev
 mkdir custom_mappings
 tar.exe -xf mavenRepo/btw/community/mappings/%MAPPINGS_VERSION%/mappings-%MAPPINGS_VERSION%.jar -C custom_mappings
-java -jar libs/tiny-remapper-0.8.6+local-fat.jar %~f1 "BTW_dev/%~nx1" custom_mappings/mappings/mappings.tiny intermediary named %userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar"
+del "%userprofile%\.gradle\caches\fabric-loom\1.6.4\minecraft-merged-intermediary.jar"
+java -jar libs/tiny-remapper-0.8.6+local-fat.jar "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/intermediary-v2.tiny" official intermediary
+java -jar libs/tiny-remapper-0.8.6+local-fat.jar "%~f1" "BTW_dev/%~nx1" custom_mappings/mappings/mappings.tiny intermediary named "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar"
 tar.exe -xf %userprofile%/.gradle/caches/fabric-loom/minecraftMaven/net/minecraft/minecraft-merged/1.6.4-btw.community.mappings.1_6_4.%MAPPINGS_VERSION%-v2/minecraft-merged-1.6.4-btw.community.mappings.1_6_4.%MAPPINGS_VERSION%-v2.jar -C BTW_dev
 tar.exe -xf "BTW_dev/%~nx1" -C BTW_dev
 del BTW_dev\%~nx1
