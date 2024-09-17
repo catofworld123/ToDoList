19321a1e360ed11f4287e97c461ef2e1ef1c2ef7
Arminias <arminias@t-online.de>
2024-09-17 20:20:27 +0200

    Small install script fixes

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 set MAPPINGS_VERSION=1.0.3
-cd "%~dp0"
+cd /d "%~dp0"
 rd /s /q "build_BTW\BTW_dev"
 rd /s /q "build_BTW\tmp\BTW_dev"
 call gradlew.bat --no-daemon downloadAssets
@@ -8,9 +8,9 @@ mkdir custom_mappings
 mkdir build_BTW\tmp\BTW_dev
 tar.exe -xf mavenRepo/btw/community/mappings/%MAPPINGS_VERSION%/mappings-%MAPPINGS_VERSION%.jar -C custom_mappings
 del "%userprofile%\.gradle\caches\fabric-loom\1.6.4\minecraft-merged-intermediary.jar"
-java -jar libs/tiny-remapper-0.8.6+local-fat.jar "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/intermediary-v2.tiny" official intermediary
+java -jar libs/tiny-remapper-0.8.6+local-fat.jar "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/intermediary-v2.tiny" official intermediary >nul
 tar.exe -xf "%~f1" -C build_BTW/tmp/BTW_dev
-java -jar libs/tiny-remapper-0.8.6+local-fat.jar "build_BTW/tmp/BTW_dev/BTW-CE-Intermediary.zip" "build_BTW/BTW_dev/BTW-CE-Intermediary.zip" custom_mappings/mappings/mappings.tiny intermediary named "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar"
+java -jar libs/tiny-remapper-0.8.6+local-fat.jar "build_BTW/tmp/BTW_dev/BTW-CE-Intermediary.zip" "build_BTW/BTW_dev/BTW-CE-Intermediary.zip" custom_mappings/mappings/mappings.tiny intermediary named "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" >nul
 tar.exe -xf %userprofile%/.gradle/caches/fabric-loom/minecraftMaven/net/minecraft/minecraft-merged/1.6.4-btw.community.mappings.1_6_4.%MAPPINGS_VERSION%-v2/minecraft-merged-1.6.4-btw.community.mappings.1_6_4.%MAPPINGS_VERSION%-v2.jar -C build_BTW/BTW_dev
 tar.exe -xf "build_BTW/BTW_dev/BTW-CE-Intermediary.zip" -C build_BTW/BTW_dev
 del build_BTW\BTW_dev\BTW-CE-Intermediary.zip
