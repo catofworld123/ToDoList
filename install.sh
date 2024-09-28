f4fbf7c1e43aefd93df0eb8ae71d19980607733b
Arminias <arminias@t-online.de>
2024-09-28 16:38:10 +0200

    Linux installation script

--- install.sh
+++ install.sh
@@ -32,15 +32,13 @@ mkdir -p build_BTW/tmp/BTW_dev
 # Extract the mappings jar using unzip
 unzip -o mavenRepo/btw/community/mappings/$MAPPINGS_VERSION/mappings-$MAPPINGS_VERSION.jar -d custom_mappings
 
-echo "Please wait..."
-
 # Delete the existing intermediary jar
 rm -f "$HOME/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar"
 
 # Run the Tiny Remapper
 java -jar libs/tiny-remapper-0.8.6+local-fat.jar "$HOME/.gradle/caches/fabric-loom/1.6.4/minecraft-merged.jar" \
     "$HOME/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" \
-    "$HOME/.gradle/caches/fabric-loom/1.6.4/intermediary-v2.tiny" official intermediary > /dev/null
+    "$HOME/.gradle/caches/fabric-loom/1.6.4/intermediary-v2.tiny" official intermediary
 
 # Extract the first argument (equivalent to %~f1) using unzip into tmp/BTW_dev
 unzip -o "$1" -d build_BTW/tmp/BTW_dev
@@ -48,7 +46,7 @@ unzip -o "$1" -d build_BTW/tmp/BTW_dev
 # Run Tiny Remapper on the extracted file
 java -jar libs/tiny-remapper-0.8.6+local-fat.jar "build_BTW/tmp/BTW_dev/BTW-CE-Intermediary.zip" \
     "build_BTW/BTW_dev/BTW-CE-Intermediary.zip" custom_mappings/mappings/mappings.tiny intermediary named \
-    "$HOME/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" > /dev/null
+    "$HOME/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar"
 
 # Extract another Minecraft jar using unzip
 unzip -o "$HOME/.gradle/caches/fabric-loom/minecraftMaven/net/minecraft/minecraft-merged/1.6.4-btw.community.mappings.1_6_4.$MAPPINGS_VERSION-v2/minecraft-merged-1.6.4-btw.community.mappings.1_6_4.$MAPPINGS_VERSION-v2.jar" -d build_BTW/BTW_dev
