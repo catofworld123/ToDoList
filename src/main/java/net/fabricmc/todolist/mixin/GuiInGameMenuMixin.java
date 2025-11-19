37516a37335893e7dd159c536ba322a827e0e9f4
catofworld123 <Karacharov123@yandex.ru>
2025-11-19 19:52:57 +0500

    update tiem!

--- src/main/java/net/fabricmc/todolist/mixin/GuiInGameMenuMixin.java
+++ src/main/java/net/fabricmc/todolist/mixin/GuiInGameMenuMixin.java
@@ -7,21 +7,23 @@ import net.minecraft.src.GuiButton;
 import net.minecraft.src.GuiIngameMenu;
 import net.minecraft.src.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiIngameMenu.class)
+@Mixin(value = GuiIngameMenu.class,priority = 1500)
 public class GuiInGameMenuMixin extends GuiScreen {
 
-    @Inject(method = "initGui", at = @At("TAIL"))
+    @Inject(method = "initGui", at = @At("RETURN"))
     private void registerMyButton(CallbackInfo ci){
         this.buttonList.add(new GuiButton(14, this.width / 2 - 100, this.height / 4 + 24 + 32,"To Do List Settings"));
     }
-    @Inject(method = "actionPerformed", at = @At("TAIL"))
+    @Inject(method = "actionPerformed", at = @At("RETURN"),cancellable = true)
     private void myButtonclic(GuiButton par1GuiButton, CallbackInfo ci){
         if (par1GuiButton.id == 14){
             this.mc.displayGuiScreen(new GuiListManager(this));
+            ci.cancel();
         }
     }
 
